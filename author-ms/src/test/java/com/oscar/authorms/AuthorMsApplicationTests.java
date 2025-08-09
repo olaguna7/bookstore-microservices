21a196cc54e59bf411fa068eb1ece5b80fe2a565
olaguna7 <oscarlagunacaminero@gmail.com>
2025-08-09 21:24:57 +0200

    first commit

---
+++ author-ms/src/test/java/com/oscar/authorms/AuthorMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscar.authorms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
